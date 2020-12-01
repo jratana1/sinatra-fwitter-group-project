400c301dcc722d884788093fe9813c422f22e8c2
John Ratana <jratana1@gmail.com>
2020-12-01 00:11:31 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,72 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if Helpers.is_logged_in?(session)
+            @tweets = Tweet.all
+            erb :'tweets/index'
+        else
+            redirect '/login'
+        end
+    end
 
+    get '/tweets/new' do
+        if Helpers.is_logged_in?(session)
+            erb :'tweets/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/tweets' do
+        
+            if params[:content].empty?
+                redirect '/tweets/new'
+            end
+        @tweet = Tweet.create(params)
+        @user = User.find(session[:user_id])
+        @user.tweets << @tweet
+        redirect "tweets/#{@tweet.id}"
+    end
+
+    get '/tweets/:id' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find(params[:id])
+            erb :'tweets/show'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find(params[:id])
+            erb :'tweets/edit'
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+      
+        @tweet = Tweet.find(params[:id])
+        if params[:content].empty?
+            redirect "/tweets/#{@tweet.id}/edit"
+        end
+        if Helpers.is_logged_in?(session) && @tweet.user.id == session[:user_id]
+            @tweet.update(:content => params[:content])
+            redirect "/tweets/#{@tweet.id}"
+        else
+            redirect "/login"
+        end
+    end
+
+    delete '/tweets/:id/delete' do
+        @tweet = Tweet.find(params[:id])
+        if Helpers.is_logged_in?(session) && @tweet.user.id == session[:user_id]
+            @tweet.destroy
+            redirect "/tweets"
+        else
+            redirect "/tweets/#{@tweet.id}"
+        end
+    end
 end
---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,26 @@
+module Slugifiable
+    module InstanceMethods
+      def slug
+        user_name = self.username
+        slug = user_name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+      end
+    end
+  
+    module ClassMethods
+      def find_by_slug(slug)
+        @slug = slug
+        format_slug_beginning
+        results = self.where("username LIKE ?", @short_slug)
+        results.detect do |result|
+          result.slug === @slug
+        end
+      end
+  
+      def format_slug_beginning
+        slug_beginning = @slug.split("-")[0]
+        slug_beginning.prepend("%")
+        slug_beginning << "%"
+        @short_slug = slug_beginning
+      end
+    end
+  end
