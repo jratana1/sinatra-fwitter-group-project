400a550a944c63cbbbac73501463152e70c7221e
John Ratana <jratana1@gmail.com>
2020-12-01 00:11:26 -0500

    commit tree d22094c01ab71923de8df01e0a31eb1f77e87fea

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if Helpers.is_logged_in?(session)
+            redirect "/tweets"
+        else
+            erb :"users/new"
+        end
+    end
 
+    post "/signup" do 
+        user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+     
+        if params[:username] == "" || params[:password] == "" || params[:email] == ""
+            redirect "/signup"
+        else  
+            user.save
+            session[:user_id] = user.id
+            redirect "/tweets"
+        end
+    end
+
+    get '/login' do
+        if Helpers.is_logged_in?(session)
+            redirect "/tweets"
+        else
+            erb :"users/login"
+        end
+    end
+
+    post "/login" do
+        user = User.find_by(:username => params[:username])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect "/tweets"
+            else
+                redirect "/"
+            end
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/login"
+    end
+
+    get "/users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        @tweets = @user.tweets
+        erb :'users/show'
+    end
 end
